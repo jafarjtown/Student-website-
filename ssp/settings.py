115a57ee49a0d421d5d99d4d58bc4c9ccf5204a2
jafarjtown <jafaridris82@gmail.com>
2023-08-22 18:22:15 +0100

    allowed host

--- ssp/settings.py
+++ ssp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-86hriykb!r6npkszi0e*t(fvg8s*ppb#8v*ze0+&el@^0+(sk6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
