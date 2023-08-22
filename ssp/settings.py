ad296f9888a1be4f96442082f86cdc9762ecc53f
jafarjtown <jafaridris82@gmail.com>
2023-08-22 19:04:44 +0100

    remove dj db

--- ssp/settings.py
+++ ssp/settings.py
@@ -77,21 +77,21 @@ WSGI_APPLICATION = 'ssp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-if not DEBUG:
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(
-            # Feel free to alter this value to suit your needs.
-            default='postgresql://postgres:postgres@localhost:5432/app',
-            conn_max_age=600
-        )
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+#if not DEBUG:
+#    import dj_database_url
+#    DATABASES = {
+#        'default': dj_database_url.config(
+#            # Feel free to alter this value to suit your needs.
+#            default='postgresql://postgres:postgres@localhost:5432/app',
+#            conn_max_age=600
+#        )
+#    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
