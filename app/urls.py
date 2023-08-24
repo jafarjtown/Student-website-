685b185f3e58bfee3ba949d8a3d7fe62c275a068
jafarjtown <jafaridris82@gmail.com>
2023-08-24 08:18:10 +0100

    update index file and modify urls

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('upload', views.upload, name='upload'),
-    path('material_list/', views.material_list, name='material_list'),   
-    path('search_materials/', views.search_materials, name='search'),
+    path('materials/', views.material_list, name='material_list'),   
+    path('search/materials/', views.search_materials, name='search'),
     path("api/courses/<int:dep>/<int:lev>/", views.courses_api, name="courses-api"),
     path("timetables/", views.timetable_view, name="timetables"),
     path("timetables/<int:id>/", views.timetable, name="timetable"),
